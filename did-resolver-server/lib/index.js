979fb7ebd86cc92bfc68f55994e626745f38bf45
Andrew Jones <ajones@digitalbazaar.com>
2022-01-31 10:47:14 -0500

    Add initial files.

---
+++ did-resolver-server/lib/index.js
@@ -0,0 +1,7 @@
+/*!
+ *  Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+require = require('esm')(module);
+module.exports = require('./main');
---
+++ did-resolver-server/lib/main.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+require('bedrock-express');
+require('bedrock-did-resolver-http');
+
+// load config defaults
+require('./config');
