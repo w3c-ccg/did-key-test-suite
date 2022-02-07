3b4488c8779efb2e241b71acc107a71b46e205fc
Andrew Jones <ajones@digitalbazaar.com>
2022-02-07 14:00:14 -0500

    Remove did-resolver-server in favor of veres-resolver.

--- did-resolver-server/lib/index.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
- */
-'use strict';
-
-require = require('esm')(module);
-module.exports = require('./main');
--- did-resolver-server/lib/main.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
- */
-'use strict';
-
-require('bedrock-express');
-require('bedrock-did-resolver-http');
-require('../config/server');
