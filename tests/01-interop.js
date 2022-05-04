dd3b49193215593220bde580ac6ffb35d2f28773
Andrew Jones <ajones@digitalbazaar.com>
2022-05-04 13:26:11 -0400

    Remove implementations dir & class.

--- tests/01-interop.js
+++ tests/01-interop.js
@@ -4,86 +4,68 @@
 'use strict';
 
 const chai = require('chai');
-const Implementation = require('./Implementation');
 const dids = require('../dids');
-const implementations = require('../implementations');
+const {filterImplementations} = require('vc-api-test-suite-implementations');
 
 const should = chai.should();
-// test these implementations' issuers or resolvers
-const test = [
-  'Digital Bazaar',
-  'Danube Tech'
-];
 
-// only test listed implementations
-const testAPIs = implementations.filter(v => test.includes(v.name));
+const {match, nonMatch} = filterImplementations({filter: ({value}) => {
+  return value.didResolvers.some(
+    didResolver => didResolver.tags.has('Did-Key'));
+}});
 
 describe('did:key Method Tests', function() {
   const summaries = new Set();
   this.summary = summaries;
-  // column names for the matrix go here
-  const columnNames = [];
   const reportData = [];
   // this will tell the report
   // to make an interop matrix with this suite
   this.matrix = true;
   this.report = true;
-  this.columns = columnNames;
+  this.columns = [...match.keys()];
+  this.notImplemented = [...nonMatch.keys()];
   this.rowLabel = 'Test Name';
-  this.columnLabel = 'DiD Key Resolver';
+  this.columnLabel = 'Did Key Resolver';
   // the reportData will be displayed under the test title
   this.reportData = reportData;
-  for(const resolver of testAPIs) {
-    // wrap the testApi config in an Implementation class
-    const implementation = new Implementation(resolver);
-    describe(resolver.name, function() {
-      columnNames.push(resolver.name);
+  for(const [name, implementation] of match) {
+    const didResolver = implementation.didResolvers.find(
+      dr => dr.tags.has('Did-Key'));
+    describe(name, function() {
       for(const test of dids) {
         it(test.title, async function() {
-          this.test.cell = {columnId: resolver.name, rowId: test.row};
+          this.test.cell = {columnId: name, rowId: test.row};
           // negative tests here
           if(test.negative === true) {
-            let error;
-            let response;
-            try {
-              response = await implementation.didResolver({did: test.did});
-            } catch(e) {
-              error = e;
-            }
-            should.not.exist(response,
+            const {result, error} = await didResolver.resolve({did: test.did});
+            should.not.exist(result,
               'Expected didResolver to return an error');
             should.exist(error);
             should.exist(error.status, 'Expected error to have a status.');
-            // FIXME change this is more exact error codes after the spec
+            // FIXME change this to more exact error codes after the spec
             // is updated.
             error.status.should.be.gte(400, 'Expected a negative status code');
             /*
             error.status.should.eql(test.expected.status,
-              'Expected didResolver response status to match expected status.');
+              'Expected didResolver result status to match expected status.');
             */
             should.exist(error.data, 'Expected error to have data.');
           }
           // positive tests here
           if(test.negative == false) {
-            let error;
-            let response;
-            try {
-              response = await implementation.didResolver({did: test.did});
-            } catch(e) {
-              error = e;
-            }
-            should.exist(response,
+            const {result, error} = await didResolver.resolve({did: test.did});
+            should.exist(result,
               'Expected didResolver to return a response.');
             should.not.exist(error, 'Expected no errors from didResolver.');
-            response.status.should.eql(test.expected.status,
+            result.status.should.eql(test.expected.status,
               'Expected didResolver response status to match expected status.');
-            should.exist(response.data,
+            should.exist(result.data,
               'Expected didResolver response data to exist.');
-            response.data.should.be.an('object',
+            result.data.should.be.an('object',
               'Expected didResolver response data to be an object.');
-            should.exist(response.data.didDocument,
+            should.exist(result.data.didDocument,
               'Expected a didDocument in the response data.');
-            response.data.didDocument.should.eql(test.expected.didDocument,
+            result.data.didDocument.should.eql(test.expected.didDocument,
               'Expected response didDocument to match expected didDocument.');
           }
         });
